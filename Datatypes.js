46c54379690670a4cf4ecffe65ea2e37d960e2ea
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-11 18:59:59 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,11 @@
+/*There are two Datatypes
+1). Primitive(nn ss bb u) : null,number,string,symbol,boolean,bigint,undefined
+2). Non-Primitive: Objects*/
+a = null;
+b = 23;
+c = "Hello user";
+d = Symbol("hello this is symbol");
+e = true;
+f = BigInt(1345+78);
+g = undefined;/* or let g; */
+console.log(a,b,c,d,e,f,g);
