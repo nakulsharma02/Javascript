e68df9959413e8d50d2d44c7484d9fcc206d5ab9
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-11 17:03:08 +0530

    Variables

---
+++ Variables.js
@@ -0,0 +1,13 @@
+console.log("hello User")
+var a = 10;
+console.log("a",a);
+var a= 90;
+console.log("a",a);
+const b = 67;
+console.log("b",b);
+let c = 12;
+console.log("c",c)
+{
+    let c = 90;
+    console.log("c",c);
+}
