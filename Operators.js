c80cc413dd370a8113a7c23331b14cd33c1a7dfb
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-11 23:51:41 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,62 @@
+/* Types of Operators :-
+1). Arithmetic Operators = +, -, *, /, **, %, --, ++ 
+2). Assignment Operators = =, +=, -=, /=, *=, **=, %= 
+3). Conditional Operators = ==, !=, ===, !==, <, >, >=, <=
+4). Logical Operators = &&, ||, !
+5). Bitwise Operators = &, |, ^, ~, >>, <<, >>>
+6). Ternary Operator = Condition ? expressionIftrue: expressionIffalse */
+let a = 43;
+let b = 4;
+console.log("a",a);
+console.log("b",b);
+
+// Arithmetic Operators
+/*console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a**b",a**b);
+console.log("a%b",a%b);
+console.log("a--",a--);
+console.log("a",a);
+console.log("++b",++b);
+console.log("b",b);*/
+
+//Assignment Operators
+let c=1;
+c+=a;
+/*console.log("c",c);
+let d =7;
+d-=b;
+console.log("d",d);
+console.log("c/=d",c/=d);
+console.log("c*=d",c*=d);
+console.log("c**=d",c**=d);
+console.log("c%=d",c%=d);
+*/
+
+// Conditional Operators
+/*console.log(a==b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a===b);
+console.log(a!==b);*/
+
+// Logical Operators
+/* let x = true;
+ let y = false;
+ console.log(x==y || x!=y);
+ console.log(x==y && x!=y);
+ console.log(!x)*/
+ 
+ 
+
+
+
+
+
+
+
