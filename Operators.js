71831c0fa4d86e2d8f82800243e1b0f4fc5b3184
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-12 10:19:57 +0530

    Operator

--- Operators.js
+++ Operators.js
@@ -5,10 +5,10 @@
 4). Logical Operators = &&, ||, !
 5). Bitwise Operators = &, |, ^, ~, >>, <<, >>>
 6). Ternary Operator = Condition ? expressionIftrue: expressionIffalse */
-let a = 43;
-let b = 4;
-console.log("a",a);
-console.log("b",b);
+// let a = 43;
+// let b = 4;
+// console.log("a",a);
+// console.log("b",b);
 
 // Arithmetic Operators
 /*console.log("a+b",a+b);
@@ -23,8 +23,8 @@ console.log("++b",++b);
 console.log("b",b);*/
 
 //Assignment Operators
-let c=1;
-c+=a;
+// let c=1;
+// c+=a;
 /*console.log("c",c);
 let d =7;
 d-=b;
@@ -52,11 +52,21 @@ console.log(a!==b);*/
  console.log(x==y && x!=y);
  console.log(!x)*/
  
- 
+ // Bitwise Operators
+ /*let x = 12;
+ console.log(x);
+ let y =90;
+ console.log(y);
+ console.log(x<y & x!=0);
+ console.log(x>y | x!=0);
+ console.log(x^y);
+ console.log(~x);
+ console.log(x>>1);
+ console.log(x<<2);
+ console.log(x>>>2);*/
 
-
-
-
-
-
+ // Ternary Operator
+ /*let x = 23;
+ let y = 90;
+ console.log(x<y?(x+y):(x-y));*/
 
