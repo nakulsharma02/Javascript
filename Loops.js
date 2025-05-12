f9f19d3b645a81971c4edf9f25fd6c5ec8195771
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 00:32:45 +0530

    Loops

---
+++ Loops.js
@@ -0,0 +1,43 @@
+//loops: for, for - in ,for - of, while, do while
+//for loop
+/*n = prompt("Enter the number");
+n = Number.parseInt(n);
+factorial = 1
+for (let i=n; i>0;i--){
+    factorial*=i;
+}
+console.log("The factorial of n is"+" "+factorial);
+*/
+
+// for  - in loop
+/*let obj = {
+    nakul : 20,
+    bhola : 16,
+    pulkit : 19
+}
+for (const i in obj) {
+        console.log(i,obj[i]);
+}*/
+
+// for - of loop
+/*for (const i of "Hello") {
+    console.log(i);
+}
+*/
+
+//while loop
+/*let n = 9;
+let sum = 0;
+let i = 1;
+while(i<n){
+    sum+=i;
+    i++
+}
+console.log(sum);*/
+
+// do while loop
+let i = 0
+do{ 
+ console.log(i);
+ i++
+}while(i<10);
