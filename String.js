400594ba8c2f8eaa845f97daa8ac366ba13ca9d0
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 23:52:37 +0530

    String

---
+++ String.js
@@ -0,0 +1,34 @@
+/*let string = "Hello World ";
+console.log(string);
+let name = 'Raman';
+console.log(name);
+console.log(string+name);
+let n2 = `name2`;
+console.log(n2);*/
+
+//Template Literals
+/*console.log("Hello World\"s");
+console.log('Hello World\'s');
+console.log(('Hello World\'s').length);
+console.log(`Hello World\`s`);
+const n3 = "Nakul";
+const message = `${n3} you are smarter than others.`;//string Interpolation using only in(``)
+console.log(message);*/
+
+//String properties 
+/*let string = "Hello String"
+console.log(string.length);
+console.log(string.constructor);*/
+ 
+// String Methods
+let string = "  Hello Dosto";
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+console.log(string.trim());
+console.log(string.slice(2,5));
+console.log(string.replace('Hello','Hii'));
+console.log(string.endsWith("Dosto"));
+console.log(string.startsWith("Hello"));
+let str1 = "Hello"
+console.log(string.includes(str1));
+console.log(string.indexOf("D"));
