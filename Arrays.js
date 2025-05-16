507915962b379b489169d00ee7ea70e106abfe5c
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-16 23:56:47 +0530

    Array Updated

--- Arrays.js
+++ Arrays.js
@@ -5,19 +5,38 @@ console.log(array.length);
 // console.log(array);
 
 //Array Methods
-console.log(array.toString());
-console.log(array.join('------'));
-let a = array.push("Nine");
-console.log(array);
-console.log(a);
-let b = array.pop();
-console.log(array);
-console.log(b);
-let c = array.shift();
-console.log(array);
-console.log(c);
-let d = array.unshift("a");
-console.log(array);
-console.log(d)
-delete array[6];
-console.log(array);
+// console.log(array.toString());
+// console.log(array.join('------'));
+// let a = array.push("Nine");
+// console.log(array);
+// console.log(a);
+// let b = array.pop();
+// console.log(array);
+// console.log(b);
+// let c = array.shift();
+// console.log(array);
+// console.log(c);
+// let d = array.unshift("a");
+// console.log(array);
+// console.log(d)
+// delete array[6];
+// let compare = (a,b)=>{
+//     return a-b;
+// }
+// console.log(array);
+// let arr1 = [4,26,30,9,7];
+// console.log(array.concat(arr1));
+// console.log(arr1.sort(compare));// Sorted array alphabetically taking all numbers as string.
+// console.log(array.reverse());
+
+// Splice and Slice
+//Splice Method
+let num  = [12,34,67,89];
+console.log(num);
+// let deletedval = num.splice(1,2,56,78,99); (index,deleting values,adding values)// the splice method returnd deleted values
+// console.log(num);
+// console.log(deletedval);
+
+//Slice Method
+let num1 = num.slice(1,3);
+console.log(num1);
